3777c643af2aa873f3034638dfd4d2d2821e6171
LightDye08 <chaostrejo5@gmail.com>
2026-03-10 15:12:48 -0600

    Contexto del Producto dentro de la base de datos

---
+++ backend/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ProductosApp.Models;
+
+namespace ProductosApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+        public DbSet<Producto> Productos => Set<Producto>();
+    }
+}
